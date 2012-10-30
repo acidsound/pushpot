c730a8ab4507c862aa740d217fbc56bfffb594a4
acidsound <spectrick@gmail.com>
2012-10-30 19:23:27 +0900

    raw 파일 오류 수정

--- routes/index.js
+++ routes/index.js
@@ -1,18 +1,18 @@
 var fs = require('fs');
 var uuid = require('node-uuid');
-exports.index = function(req, res){
-  var data = !!req.files ? fs.readFileSync(req.files.fileupload.path) : req.method==='POST' ? JSON.stringify(req.body) : req.query['data'];
+exports.index = function (req, res) {
+  var data = !!req.files ? fs.readFileSync(req.files.fileupload.path) : req.method === 'POST' ? JSON.stringify(req.body) : req.query['data'];
   var id = req.params['uuid'] || uuid.v1().split('-').join('');
-  if (!!data) fs.writeFileSync(__dirname + '/../repo/'+id+'.json', data);
+  if (!!data) fs.writeFileSync(__dirname + '/../repo/' + id + '.json', data);
   res.json(req.params['uuid'] ? {"result":"ok"} : {"id":id});
 };
-exports.read = function(req, res){
+exports.read = function (req, res) {
   var renderResult = function (err, data) {
     try {
       res.json(JSON.parse(data.toString()));
     } catch (e) {
-      res.send(text);
+      res.send(data);
     }
   };
-  fs.readFile(__dirname + '/../repo/'+req.params.uuid+'.json', renderResult);
+  fs.readFile(__dirname + '/../repo/' + req.params.uuid + '.json', renderResult);
 };
